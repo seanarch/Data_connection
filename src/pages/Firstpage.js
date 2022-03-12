a5c2409bb601ee16d09ed586dacdc990b88fdea3
Sean <seanzhu@Seans-Mac-mini.local>
2022-03-12 15:44:28 -0700

    Update webpage

--- src/pages/Firstpage.js
+++ src/pages/Firstpage.js
@@ -5,143 +5,97 @@ import { Container, Grid, InputLabel, Select, MenuItem, FormControl } from '@mat
 import TextField from '../components/TextField/TextField';
 import DatePicker from '../components/DatePicker';
 import { TextareaAutosize } from '@mui/base';
+import { useState, useEffect } from 'react';
 import axios from "axios"; 
+
+ 
+
+function Firstpage() {
+    // enteredID can be the params that passed from the previous page
+    const enteredID = 2;
+    const [APIData, setAPIData] = useState([]);
+     
+
+    useEffect(() => {
+      axios.get(`http://localhost:3010/patients`, { params:{id:enteredID}})
+        .then((response) => {
+            setAPIData(response.data)
+        })
+    }, [])
+
+    // const selectedData = APIData.filter((enteredID) => {
+
+    // })
 
     const INITIAL_VALUES = {
         info: {
             id: 1,
             date: "2000-10-10", 
-            test: "Test string",
+            comments: "Test comments",
         },
+
 
     }
 
-    const Firstpage = () => {
-        return (
-                    <Container maxWidth="md">
-        <div className='container' style={{
+
+  return (
+      
+    <div>
+        <Container maxWidth="md">
+            <div className='container' style={{
             display: 'flex', justifyContent:
                 'center', alignItems: 'center', marginTop: '50px'
-        }}>
-            <Formik initialValues={{ ...INITIAL_VALUES }} onSubmit={values => {
-                console.log(values) 
             }}>
-                {props => (
-                    <Form>
-                        
-                        <Grid container spacing={3} width={'70vw'}>
-                        <h3>Date</h3>
-                        <Grid item xs={12}>
-                                <DatePicker
-                                        fullWidth
-                                        name="info.date"
-                                        label="Date"
-                                    />
-                                </Grid>
+            <Formik initialValues={{ ...INITIAL_VALUES }} onSubmit={values => {
+                console.log(values)
+                console.log(APIData) 
+            }}>
+    {props => (
+        <Form>
+            
+            <Grid container spacing={3} width={'70vw'}>
+            <h3>Date</h3>
+            <Grid item xs={12}>
+                    <DatePicker
+                            fullWidth
+                            name="info.date"
+                            label="Date"
+                        />
+                    </Grid>
 
-                                 
+                     
 
-                        <h3>Date string</h3>
-                        <Grid item xs={12}>
-                                    <TextField
-                                        name="info.test"
-                                        label="Test String"
-                                    />
-                                </Grid>
-                        
-                        <h3>ID string</h3>
-                        <Grid item xs={12}>
-                                    <TextField
-                                        name="info.id"
-                                        label="id"
-                                    />
-                                </Grid>
+            <h3>Comments</h3>
+            <Grid item xs={12}>
+                        <TextField
+                            name="info.comments"
+                            label="Comments"
+                        />
+                    </Grid>
+            
+            <h3>ID </h3>
+            <Grid item xs={12}>
+                        <TextField
+                            name="info.id"
+                            label="id"
+                        />
+                    </Grid>
 
-                                <Grid item xs={12}>
-                                    <Button color='primary' type="submit">Save</Button>
-                                </Grid>
-                            </Grid>
-                        
-                    </Form>
-                )}
-            </Formik>
-        </div>
-    </Container>
-        )}
-    
+                    <Grid item xs={12}>
+                        <Button color='primary' type="submit">Save</Button>
+                    </Grid>
+                </Grid>
+            
+        </Form>
+    )}
+</Formik>
+</div>
+</Container>
 
-// function Firstpage() {
+ 
 
-//     const [data, setData] = useState(""); 
-
-//     const INITIAL_VALUES = {
-//         info: {
-
-//             id: "1",
-//             date: "2021-10-10",
-//             datestring: "2020-01-01",
-
-//         }
-
-//     }
-
-
-//   return (
-      
-//     <div>
-
-//         <Container maxWidth="md">
-//         <div className='container' style={{
-//             display: 'flex', justifyContent:
-//                 'center', alignItems: 'center', marginTop: '50px'
-//         }}>
-//             <Formik initialValues={{ ...INITIAL_VALUES }} onSubmit={values => {
-//                 console.log(values) 
-//             }}>
-//                 {props => (
-//                     <Form>
-                        
-//                         <Grid container spacing={3} width={'70vw'}>
-//                         <h3>Date date</h3>
-//                         <Grid item xs={12}>
-//                                 <DatePicker
-//                                         fullWidth
-//                                         name="info.date"
-//                                         label="Date"
-//                                     />
-//                                 </Grid>
-
-                                 
-
-//                         <h3>Date string</h3>
-//                         <Grid item xs={12}>
-//                                     <TextField
-//                                         name="info.datestring"
-//                                         label="Date String"
-//                                     />
-//                                 </Grid>
-                        
-//                         <h3>ID string</h3>
-//                         <Grid item xs={12}>
-//                                     <TextField
-//                                         name="info.id"
-//                                         label="ID"
-//                                     />
-//                                 </Grid>
-
-//                                 <Grid item xs={12}>
-//                                     <Button color='primary' type="submit">Save</Button>
-//                                 </Grid>
-//                             </Grid>
-                        
-//                     </Form>
-//                 )}
-//             </Formik>
-//         </div>
-//     </Container>
-
-//     </div>
-//   )
-// }
+    </div>
+  )
+}
 
 export default Firstpage;
--- src/pages/Searchpage.js
+++ src/pages/Searchpage.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from 'react';
+import { Formik, Form } from 'formik';
+import { Button } from 'reactstrap';
+import { Container, Grid, InputLabel, Select, MenuItem, FormControl } from '@material-ui/core';
+import TextField from '../components/TextField/TextField';
+import DatePicker from '../components/DatePicker';
+import { TextareaAutosize } from '@mui/base';
+import { useState, useEffect } from 'react';
+import axios from "axios"; 
+
 
 function Searchpage() {
+
+  const INITIAL_VALUES = {
+    data: {
+        id: 0,
+    },
+
+
+}
+
   return (
-    <div>Searchpage</div>
+  
+
+<div>
+        <Container maxWidth="md">
+            <div className='container' style={{
+            display: 'flex', justifyContent:
+                'center', alignItems: 'center', marginTop: '50px'
+            }}>
+            <Formik initialValues={{ ...INITIAL_VALUES }} onSubmit={values => {
+                console.log(values)
+                 
+            }}>
+    {props => (
+        <Form>
+            
+            <Grid container spacing={3} width={'70vw'}>
+            <h3>Data in this page</h3>
+             
+ 
+            <Grid item xs={12}>
+                        <TextField
+                            name="data.id"
+                            label="id"
+                        />
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <Button color='primary' type="submit">Save</Button>
+                    </Grid>
+                </Grid>
+            
+        </Form>
+    )}
+</Formik>
+</div>
+</Container>
+
+ 
+
+    </div>
   )
 }
+
+     
 
 export default Searchpage
